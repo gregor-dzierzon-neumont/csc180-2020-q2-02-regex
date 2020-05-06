cecc12543da5f871bf69a8716b88be3be28f7ce2
Gregor Dzierzon <gdzierzon@gmail.com>
2020-05-06 13:20:49 -0600

    added calculator project files

---
+++ Module 04/CalculatorInterfaces/src/main/java/edu/neumont/Calculator.java
@@ -0,0 +1,9 @@
+package edu.neumont;
+
+public interface Calculator
+{
+    double add(double a, double b);
+    double subtract(double a, double b);
+    double multiply(double a, double b);
+    double divide(double a, double b);
+}
